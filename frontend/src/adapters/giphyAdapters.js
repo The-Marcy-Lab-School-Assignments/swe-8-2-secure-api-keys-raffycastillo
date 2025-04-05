b5a226d774757d3be075c4d21cf369b3229afb39
raffycastillo <78766072+raffycastillo@users.noreply.github.com>
2025-04-05 13:41:58 -0400

    done

--- frontend/src/adapters/giphyAdapters.js
+++ frontend/src/adapters/giphyAdapters.js
@@ -1,12 +1,10 @@
 import { handleFetch } from './handleFetch.js'
 
-const API_KEY = 'How can I include this without exposing it to the public?'
-
 // Send a fetch request to the /trending endpoint and return the top 3 results
 export const getTrendingGifs = async () => {
-  return await handleFetch(`https://api.giphy.com/v1/gifs/trending?api_key=${API_KEY}&limit=3`)
+  return await handleFetch(`http://localhost:8080/api/gifs`)
 }
 
 export const getGifsBySearch = async (searchTerm) => {
-
+  return await handleFetch(`http://localhost:8080/api/gifs?search=${searchTerm}`)
 }
--- server/index.js
+++ server/index.js
@@ -4,6 +4,9 @@
 
 const path = require('path');
 const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
+const { handleFetch } = require('./handleFetch')
 
 //////////////////////////
 // Constants
@@ -21,8 +24,19 @@ const serveStatic = express.static(pathToDistFolder);
 
 app.use(serveStatic);
 
+const giphyBaseURL = `https://api.giphy.com/v1/gifs`;
+
+app.get('/api/gifs', async (req, res) => {
+  const { search } = req.query;
+  let data, error;
+  if (!search) [data, error] = await handleFetch(`${giphyBaseURL}/trending?api_key=${process.env.API_KEY}&limit=3`);
+  else [data, error] = await handleFetch(`${giphyBaseURL}/search?api_key=${process.env.API_KEY}&q=${search}&limit=3`);
+  if (error) return res.status(404).send(error);
+  return res.status(200).send(data);
+})
+
 //////////////////////////
 // Listener
 //////////////////////////
 
-app.listen(port, () => console.log(`listening at http://localhost:${port}`)); 
+app.listen(port, () => console.log(`listening at http://localhost:${port}`));
