2bafe8372d256008aa1483dbcdd45f7f42d1c596
raffycastillo <78766072+raffycastillo@users.noreply.github.com>
2025-04-05 13:48:06 -0400

    vite config for deploy

--- frontend/src/adapters/giphyAdapters.js
+++ frontend/src/adapters/giphyAdapters.js
@@ -2,9 +2,9 @@ import { handleFetch } from './handleFetch.js'
 
 // Send a fetch request to the /trending endpoint and return the top 3 results
 export const getTrendingGifs = async () => {
-  return await handleFetch(`http://localhost:8080/api/gifs`)
+  return await handleFetch(`/api/gifs`)
 }
 
 export const getGifsBySearch = async (searchTerm) => {
-  return await handleFetch(`http://localhost:8080/api/gifs?search=${searchTerm}`)
+  return await handleFetch(`/api/gifs?search=${searchTerm}`)
 }
